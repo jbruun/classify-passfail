96761400c1f31fd584d672aaacb61875bc865a51
Adrienne Traxler <adrienne.traxler@gmail.com>
2020-09-04 08:36:53 -0400

    Move logistic regression jackknife loop to central file

--- code/passfail_logreg_jackknife.R
+++ code/passfail_logreg_jackknife.R
@@ -20,61 +20,17 @@ centPS <- dfPS %>% group_split(Week)
 centCD <- dfCD %>% group_split(Week)
 centICS <- dfICS %>% group_split(Week)
 
-# Input: List of weekly data frames, optional outcome (pass/justpass), optional 
-#  subset of predictors to use
-# Output: List of prediction vectors for that weekly aggregate network; each node in the 
-#  vector is predicted using all the other nodes
-jackPred <- function(layer, outcome = "pass", 
-                     predictors = c("gender", "cohort", "fci_pre", "PageRank", 
-                                    "tarEnt", "Hide")) {
-  if (outcome == "pass" | outcome == "justpass") {
-    choices <- c("0", "1")
-  } else {
-    stop("Not a valid outcome variable.")
-  }
-  # remove incomplete rows
-  userows <- complete.cases(layer[[length(layer)]][, c(outcome,predictors)])  
-
-  allprob <- matrix(nrow = sum(userows), ncol = length(layer))
-  fitStr <- paste(predictors, collapse = " + ")
-  fitForm <- paste0(outcome, " ~ ", fitStr)
-  for(j in 1:length(layer)) {
-    # data is complete cases
-    data <- layer[[j]][userows, c(outcome, predictors)]
-    # Loop through all nodes
-    for(i in 1:dim(data)[1]) {
-      # training set is data minus observation i
-      train <- data[-i, ]
-      glm.fit <- glm(fitForm, family = binomial, data = train)
-      allprob[i, j] <- predict(glm.fit, newdata = data[i, ], type = "response")
-    }
-  }
-  allpred <- allprob
-  allpred[allprob < 0.5] <- choices[1]    # 0
-  allpred[allprob >= 0.5] <- choices[2]   # 1
-  
-  # To return: node name, actual outcome, predicted outcome columns
-  alldata <- data.frame(layer[[1]][userows, "name"], data[, outcome], as.data.frame(allpred))
-  
-  # Turn outcomes into factor
-  for(i in seq_along(layer)) {
-    alldata[, i+2] <- as.factor(alldata[, i+2])
-  }
-  
-  names(alldata) <- c("name", outcome, paste0("Week", c(1:length(layer))))
-  print(paste0("Fit: ", fitForm, ", complete N = ", dim(alldata)[1]))
-  return(alldata)
-}
+source("code/jackknife_functions.R")
 
 # Predict pass/fail
-predPS <- jackPred(centPS)
-predCD <- jackPred(centCD)
-predICS <- jackPred(centICS)
+predPS <- jackPredLog(centPS)
+predCD <- jackPredLog(centCD)
+predICS <- jackPredLog(centICS)
 
 # Predict just-pass/just-fail (2/0)
-predJustPS <- jackPred(centPS, outcome = "justpass")
-predJustCD <- jackPred(centCD, outcome = "justpass")
-predJustICS <- jackPred(centICS, outcome = "justpass")
+predJustPS <- jackPredLog(centPS, outcome = "justpass")
+predJustCD <- jackPredLog(centCD, outcome = "justpass")
+predJustICS <- jackPredLog(centICS, outcome = "justpass")
 
 # Save pass/fail predictions
 save(predPS, predCD, predICS, predJustPS, predJustCD, predJustICS,
@@ -112,9 +68,9 @@ write.csv(succRateJust,"succRateJust.csv", row.names = FALSE)
 ## OLD, NOT UPDATED YET 
 
 # What if we ignore FCIpre?
-predPSnoFCI <-jackPred(centPS,predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
-predCDnoFCI <-jackPred(centCD,predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
-predICSnoFCI <-jackPred(centICS,predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
+predPSnoFCI <-jackPredLog(centPS,predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
+predCDnoFCI <-jackPredLog(centCD,predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
+predICSnoFCI <-jackPredLog(centICS,predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
 succRatenoFCI <- rbind(sapply(predPSnoFCI[,3:9],function(x) mean(x==predPSnoFCI$Pass)),
                        sapply(predCDnoFCI[,3:9],function(x) mean(x==predCDnoFCI$Pass)),
                        sapply(predICSnoFCI[,3:9],function(x) mean(x==predICSnoFCI$Pass)))
@@ -123,9 +79,9 @@ succRatenoFCI <- data.frame(Layer=c("PS","CD","ICS"),N=c(dim(predPSnoFCI)[1],dim
                             Guessing=c(mean(predPSnoFCI$Pass=="Pass"),mean(predCDnoFCI$Pass=="Pass"),
                                        mean(predICSnoFCI$Pass=="Pass")))
 
-predPSnoFCI02 <-jackPred(centPS,outcome="JustPass",predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
-predCDnoFCI02 <-jackPred(centCD,outcome="JustPass",predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
-predICSnoFCI02 <-jackPred(centICS,outcome="JustPass",predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
+predPSnoFCI02 <-jackPredLog(centPS,outcome="JustPass",predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
+predCDnoFCI02 <-jackPredLog(centCD,outcome="JustPass",predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
+predICSnoFCI02 <-jackPredLog(centICS,outcome="JustPass",predictors=c("Gender","Section","PageRank","tarEnt","Hide"))
 succRatenoFCI02 <- rbind(sapply(predPSnoFCI02[,3:9],function(x) mean(x==predPSnoFCI02$JustPass)),
                        sapply(predCDnoFCI02[,3:9],function(x) mean(x==predCDnoFCI02$JustPass)),
                        sapply(predICSnoFCI02[,3:9],function(x) mean(x==predICSnoFCI02$JustPass)))
