ce12c053f3c2396c97db301a922143048b256519
Adrienne Traxler <adrienne.traxler@gmail.com>
2020-09-04 08:48:06 -0400

    Move LDA jackknife loop to central file

--- code/jackknife_functions.R
+++ code/jackknife_functions.R
@@ -55,6 +55,61 @@ jackPredLog <- function(layer, outcome = "pass",
 }
 
 
+# Linear discriminant analysis (LDA) version
+# Input: List of weekly data frames, optional outcome (pass/justpass), optional 
+#  subset of predictors to use
+# Output: List of prediction vectors for that weekly aggregate network; each 
+#  node in the vector is predicted using all the other nodes
+jackPredLDA <- function(layer, outcome = "pass", 
+                        predictors = c("gender", "cohort", "fci_pre", 
+                                       "PageRank", "tarEnt", "Hide")) {
+  # Check for valid input
+  if (outcome == "pass" | outcome == "justpass") {
+    choices <- c("0", "1")
+  } else {
+    stop("Not a valid outcome variable.")
+  }
+  # Remove incomplete rows
+  userows <- complete.cases(layer[[length(layer)]][, c(outcome,predictors)])  
+  
+  allprob <- matrix(nrow = sum(userows), ncol = length(layer))
+  cases <- c(1:sum(userows))
+  
+  # Build fitting input string using predictor names
+  fitStr <- paste(predictors, collapse = " + ")
+  fitForm <- paste0(outcome, " ~ ", fitStr)
+  
+  # Loop through all weeks
+  for(j in 1:length(layer)) {
+    # Data is complete cases
+    data <- data.frame(layer[[j]][userows, c(outcome, predictors)])
+    # Loop through all nodes
+    for(i in 1:dim(data)[1]) {
+      # Training set is data minus observation i
+      train <- !cases==i
+      lda.fit <- lda(data[,-1], grouping=data[,1], subset = train)
+      allprob[i, j] <- predict(lda.fit, newdata = data[i,-1])$posterior[2]
+    }
+  }
+  allpred <- allprob
+  allpred[allprob < 0.5] <- choices[1]    # 0
+  allpred[allprob >= 0.5] <- choices[2]   # 1
+  
+  # To return: node name, actual outcome, predicted outcome columns
+  alldata <- data.frame(layer[[1]][userows, "name"], data[, outcome], 
+                        as.data.frame(allpred))
+  
+  # Turn outcomes into factor
+  for(i in seq_along(layer)) {
+    alldata[, i+2] <- as.factor(alldata[, i+2])
+  }
+  
+  names(alldata) <- c("name", outcome, paste0("Week", c(1:length(layer))))
+  print(paste0("Fit: ", fitForm, ", complete N = ", dim(alldata)[1]))
+  return(alldata)
+}
+
+
 # K nearest neighbors version
 # Input: List of weekly data frames, optional outcome (pass/justpass), optional 
 #  subset of predictors to use
--- code/passfail_lda_jackknife.R
+++ code/passfail_lda_jackknife.R
@@ -21,52 +21,7 @@ centPS <- dfPS %>% group_split(Week)
 centCD <- dfCD %>% group_split(Week)
 centICS <- dfICS %>% group_split(Week)
 
-# Input: List of weekly data frames, optional outcome (pass/justpass), optional 
-#  subset of predictors to use
-# Output: List of prediction vectors for that weekly aggregate network; each node in the 
-#  vector is predicted using all the other nodes
-jackPredLDA <- function(layer, outcome = "pass", 
-                     predictors = c("gender", "cohort", "fci_pre", "PageRank", 
-                                    "tarEnt", "Hide")) {
-  if (outcome == "pass" | outcome == "justpass") {
-    choices <- c("0", "1")
-  } else {
-    stop("Not a valid outcome variable.")
-  }
-  # remove incomplete rows
-  userows <- complete.cases(layer[[length(layer)]][, c(outcome,predictors)])  
-
-  allprob <- matrix(nrow = sum(userows), ncol = length(layer))
-  cases<-c(1:sum(userows))
-  fitStr <- paste(predictors, collapse = " + ")
-  fitForm <- paste0(outcome, " ~ ", fitStr)
-  for(j in 1:length(layer)) {
-    # data is complete cases
-    data <- data.frame(layer[[j]][userows, c(outcome, predictors)])
-    # Loop through all nodes
-    for(i in 1:dim(data)[1]) {
-      # training set is data minus observation i
-      train <- !cases==i
-      lda.fit <- lda(data[,-1], grouping=data[,1], subset = train)
-      allprob[i, j] <- predict(lda.fit, newdata = data[i,-1])$posterior[2]
-    }
-  }
-  allpred <- allprob
-  allpred[allprob < 0.5] <- choices[1]    # 0
-  allpred[allprob >= 0.5] <- choices[2]   # 1
-  
-  # To return: node name, actual outcome, predicted outcome columns
-  alldata <- data.frame(layer[[1]][userows, "name"], data[, outcome], as.data.frame(allpred))
-  
-  # Turn outcomes into factor
-  for(i in seq_along(layer)) {
-    alldata[, i+2] <- as.factor(alldata[, i+2])
-  }
-  
-  names(alldata) <- c("name", outcome, paste0("Week", c(1:length(layer))))
-  print(paste0("Fit: ", fitForm, ", complete N = ", dim(alldata)[1]))
-  return(alldata)
-}
+source("code/jackknife_functions.R")
 
 # Predict pass/fail
 predPS <- jackPredLDA(centPS)
